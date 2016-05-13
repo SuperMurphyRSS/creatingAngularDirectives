1158e02655793c9c9ad22e2379954bea2ae10233
Muftau Abdulrahamon <tunde4mons@outlook.com>
2016-05-13 19:22:32 +0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 module.controller("controller", function($scope) {
 	$scope.Info =[{
 		name :"Phill",
-	    position:"developer"
+		position:"developer"
 	}]
      $scope.name = $scope.Info[0].name;
 	 $scope.position = $scope.Info[0].position;
